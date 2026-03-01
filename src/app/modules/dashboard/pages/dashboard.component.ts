e5a53805112624ae8d1440e116c7a0a69d6d05d2
theomar309 <dj.lopezciriaco20@gmail.com>
2026-03-01 17:20:01 -0600

    Primera version del front inventory

---
+++ src/app/modules/dashboard/pages/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { Sidenav } from '../../shared/components/sidenav/sidenav';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, Sidenav],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent {
+
+}
